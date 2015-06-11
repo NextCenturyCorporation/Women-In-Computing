495b4a8037ed19297fc7d1529fd7121782c7aa72
Tina Coleman <tina.coleman@nextcentury.com>
2015-06-11 05:04:59 -0400

    Task 2: Basic doughnut

--- Minecraft/src/main/java/com/nextcentury/wic/minecraft/examplemod/ExampleMod.java
+++ Minecraft/src/main/java/com/nextcentury/wic/minecraft/examplemod/ExampleMod.java
@@ -4,17 +4,28 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import com.nextcentury.wic.minecraft.food.Doughnut;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod {
 	public static final String MODID = "examplemod";
 	public static final String VERSION = "1.0";
-
+	public static Doughnut doughnut;
+	
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		// TODO: Insert your message below...
 		System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
 
+	}
+
+	@EventHandler
+	public void preLoad(FMLPreInitializationEvent event) {
+		doughnut = new Doughnut();
+		GameRegistry.registerItem(doughnut, "doughnut");
 	}
 
 }
