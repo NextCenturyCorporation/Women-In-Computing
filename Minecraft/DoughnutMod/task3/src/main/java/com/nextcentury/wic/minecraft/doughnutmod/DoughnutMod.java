455471c7497c79999928b070b4bba504b0e37d98
Tina Coleman <tina.coleman@nextcentury.com>
2015-06-14 09:30:52 -0400

    Reorganized to have Minecraft folders per activities, as well as task layout for doughnutMod

--- Minecraft/DoughnutMod/task3/src/main/java/com/nextcentury/wic/minecraft/doughnutmod/DoughnutMod.java
+++ Minecraft/DoughnutMod/task3/src/main/java/com/nextcentury/wic/minecraft/doughnutmod/DoughnutMod.java
@@ -1,4 +1,4 @@
-package com.nextcentury.wic.minecraft.examplemod;
+package com.nextcentury.wic.minecraft.doughnutmod;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -11,8 +11,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import com.nextcentury.wic.minecraft.food.BringMeDoughnuts;
 import com.nextcentury.wic.minecraft.food.Doughnut;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod {
+@Mod(modid = DoughnutMod.MODID, version = DoughnutMod.VERSION)
+public class DoughnutMod {
 	public static final String MODID = "examplemod";
 	public static final String VERSION = "1.0";
 	public static Doughnut doughnut;
---
+++ Minecraft/DoughnutMod/task4/src/main/java/com/nextcentury/wic/minecraft/doughnutmod/DoughnutMod.java
@@ -0,0 +1,33 @@
+package com.nextcentury.wic.minecraft.doughnutmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import com.nextcentury.wic.minecraft.food.BringMeDoughnuts;
+import com.nextcentury.wic.minecraft.food.Doughnut;
+
+@Mod(modid = DoughnutMod.MODID, version = DoughnutMod.VERSION)
+public class DoughnutMod {
+	public static final String MODID = "examplemod";
+	public static final String VERSION = "1.0";
+	public static Doughnut doughnut;
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		// TODO: Insert your message below...
+		System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+
+	}
+
+	@EventHandler
+	public void preLoad(FMLPreInitializationEvent event) {
+		doughnut = new Doughnut();
+		GameRegistry.registerItem(doughnut, "doughnut");
+	}
+
+}
