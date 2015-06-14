e3f56e01ef4c040fdac482baec863da6d55d0005
Tina Coleman <tina.coleman@nextcentury.com>
2015-06-14 10:53:38 -0400

    Hmm - not yet giving the kids this class - remove its reference

--- Minecraft/DoughnutMod/task1/src/main/java/com/nextcentury/wic/minecraft/doughnutmod/DoughnutMod.java
+++ Minecraft/DoughnutMod/task1/src/main/java/com/nextcentury/wic/minecraft/doughnutmod/DoughnutMod.java
@@ -5,13 +5,11 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-import com.nextcentury.wic.minecraft.food.Doughnut;
 
 @Mod(modid = DoughnutMod.MODID, version = DoughnutMod.VERSION)
 public class DoughnutMod {
 	public static final String MODID = "examplemod";
 	public static final String VERSION = "1.0";
-	public static Doughnut doughnut;
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
